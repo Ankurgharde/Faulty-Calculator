2a1ce45db8c2e9b39fc4b840e4c0bebd33086efd
Ankurgharde <77565629+Ankurgharde@users.noreply.github.com>
2021-02-02 15:00:52 +0530

    Update Faulty Calculator.py

--- Faulty Calculator.py
+++ Faulty Calculator.py
@@ -1,12 +1,12 @@
 First_Number=int(input('Enter an First Number:'))
 Second_Number=int(input('Enter an Second Number:'))
 Operator=input("Choose Operator:")
-if (Operator=='+') and (First_Number==56) and ( Second_Number==9):
-    print('Addition of Two Number is:',77)
-elif (Operator == '*') and (First_Number == 45) and (Second_Number == 3):
-    print('Multiplication of Two Number is:',555)
-elif (Operator == '/') and (First_Number == 56) and (Second_Number == 6):
-    print('Division of Two Number is:',4)
+if (Operator=='+') and (First_Number==44) and ( Second_Number==5):
+    print('Addition of Two Number is:',80)
+elif (Operator == '*') and (First_Number == 30) and (Second_Number == 7):
+    print('Multiplication of Two Number is:',75)
+elif (Operator == '/') and (First_Number == 66) and (Second_Number == 8):
+    print('Division of Two Number is:',10)
 elif Operator==('+'):
     print('Addition of Two Number is', First_Number + Second_Number)
 elif Operator == ('*'):
